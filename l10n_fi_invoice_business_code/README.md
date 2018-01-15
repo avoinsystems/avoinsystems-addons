216294247f0335609bcc3302733a799cba71ee11
Miku Laitinen <miku.laitinen@avoin.systems>
2018-01-15 19:39:32 +0200

    Added l10n_fi_invoice_business_code bridge module

---
+++ l10n_fi_invoice_business_code/README.md
@@ -0,0 +1,7 @@
+Business ID for Finnish Invoice
+===============================
+
+#### Version: 1.0.0
+
+Adds the buyer's Business ID on the Finnish Invoice template. This is
+required in invoices with reversed VAT (eg. construction services).
---
+++ l10n_fi_invoice_business_code/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Avoin.Systems
+#    Copyright 2015-2018 Avoin.Systems
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+# noinspection PyStatementEffect
+{
+    "name": "Business ID for Finnish Invoice",
+    "version": "10.0.1.0.0",
+    "author": "Avoin.Systems",
+    "category": "Localization",
+    "website": "http://avoin.systems",
+    "images": ["static/description/icon.png"],
+    "depends": [
+        "l10n_fi_invoice",
+        "l10n_fi_business_code",
+    ],
+    "data": [
+        "views/account_invoice_templates.xml",
+    ],
+    "summary": "Y-tunnus suomalaiseen laskupohjaan",
+    "active": False,
+    "installable": True,
+    "auto_install": True,
+    "application": False
+}
