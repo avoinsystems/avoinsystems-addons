afb202acbe9d3a53da8b008f606fade08f9e165a
Miku Laitinen <miku.laitinen@gmail.com>
2019-10-24 17:57:15 +0300

    [ADD] Added backup_size_header (#13)

---
+++ backup_size_header/__manifest__.py
@@ -0,0 +1,19 @@
+# noinspection PyStatementEffect
+{
+    "name": "Backup size HTTP header",
+    "version": "1.0.0",
+    "license": "AGPL-3",
+    "summary": """
+    Adds the Content-Length header to backups taken using
+    the /web/database/backup HTTP endpoint.
+    """,
+    "data": [
+    ],
+    "author": "Avoin.Systems",
+    "website": "https://avoin.systems",
+    "category": "",
+    "depends": [
+        "web",
+    ],
+    "auto_install": False,
+}
