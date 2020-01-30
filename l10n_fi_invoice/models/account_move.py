9343a77cdb8c4a1bccd2939f434db1e65b7d89ff
Daniels Andersons <daniels.andersons@avoin.systems>
2020-01-30 18:37:34 +0200

    [MIG][FEAT] l10n_fi_invoice: rework for Odoo 13.0
    - added dependency for l10n_fi_bank_barcode and removed barcode generation functionality from l10n_fi_invoice
    - removed l10n_fi_invoice_numbers dependency
    - removed from the module account journal field include_on_invoice

--- l10n_fi_invoice/models/account_move.py
+++ l10n_fi_invoice/models/account_move.py
@@ -30,43 +30,6 @@ class AccountMove(models.Model):
             replace(' ', '-').replace(',', '').replace('--', '-')
         return filename
 
-    def _compute_barcode_string(self):
-        for invoice in self:
-            displayed_bank_accounts = invoice.company_id.partner_id.bank_ids.filtered('journal_id.include_on_invoice')
-            primary_bank_account = invoice.invoice_partner_bank_id or \
-                displayed_bank_accounts and displayed_bank_accounts[0]
-            if invoice.amount_total \
-                and primary_bank_account.acc_number \
-                and invoice.ref_number \
-                and invoice.invoice_date_due:
-                if invoice.currency_id.compare_amounts(BARCODE_AMOUNT_LIMIT, invoice.amount_total) == -1:
-                    # Amount is too large, we choose the option of replacing the amount with zeros (in accordance with
-                    # https://www.finanssiala.fi/maksujenvalitys/dokumentit/Pankkiviivakoodi-opas.pdf)
-                    # so that at least some use can be gotten from the barcode
-                    euros = "000000"
-                    cents = "00"
-                else:
-                    full_amount = invoice.currency_id.round(invoice.amount_total)
-                    euros, cents = "{:.2f}".format(full_amount).zfill(9).split('.')
-                receiver_bank_account = re\
-                    .sub("[^0-9]", "", str(primary_bank_account.acc_number))
-                ref_number_filled = invoice.ref_number.zfill(20)
-                invoice.barcode_string = '4' \
-                                      + receiver_bank_account \
-                                      + str(euros) \
-                                      + str(cents) \
-                                      + "000" + ref_number_filled \
-                                      + invoice.invoice_date_due.strftime('%y%m%d')
-            else:
-                invoice.barcode_string = False
-
-    barcode_string = fields.Char(
-        'Barcode String',
-        compute='_compute_barcode_string',
-        help=_('https://www.finanssiala.fi/maksujenvalitys/dok'
-               'umentit/Pankkiviivakoodi-opas.pdf')
-    )
-
     def invoice_print(self):
         """ Print the invoice and mark it as sent, so that we can see more
             easily the next step of the workflow
