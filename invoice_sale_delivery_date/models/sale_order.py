a5f3ba50cd29604a5913554b385a45f0a113ec2e
Atte Isopuro <atte.isopuro@avoin.systems>
2022-08-09 09:42:01 +0300

    [MIG] invoice_sale_delivery_date to 15.0

--- invoice_sale_delivery_date/models/sale_order.py
+++ invoice_sale_delivery_date/models/sale_order.py
@@ -11,8 +11,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def _prepare_invoice(self):
-        self.ensure_one()
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
-        if self.effective_date:
-            invoice_vals['date_delivered'] = self.effective_date
+        if self.effective_date:  # super ensures_one
+            invoice_vals['date_delivered'] = self.effective_date.date()
         return invoice_vals
